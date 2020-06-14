41b4d4644178ae68c676a0b72462c9b11e1832ce
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 19:35:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -7,6 +7,9 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
+player1 = "Jay-Z"
+player2 = "Beyonce"
+
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
