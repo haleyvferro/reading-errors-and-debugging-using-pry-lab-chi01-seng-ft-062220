6d314cf1c25b36ed851c82c5525c8b4d547444b8
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 19:54:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -6,11 +6,12 @@ end
 
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 
 def engage
-  puts state_log(star_date)
-  star_date = generate_star_date
+  puts state_log(date)
+  date = generate_star_date
 end
